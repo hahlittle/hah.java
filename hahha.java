4f7383ca18138f94e1448a230e3cdc0555de9bc1
hahlittle <xiaoxingxing7761@outlook.com>
2024-05-14 10:24:27 +0800

    Create hahha.java

---
+++ hahha.java
@@ -0,0 +1,60 @@
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // 输出 Hello World
+    }
+}
+class FreshJuice {
+   enum FreshJuiceSize{ SMALL, MEDIUM , LARGE }
+   FreshJuiceSize size;
+}
+ 
+public class FreshJuiceTest {
+   public static void main(String[] args){
+      FreshJuice juice = new FreshJuice();
+      juice.size = FreshJuice.FreshJuiceSize.MEDIUM  ;
+   }
+}
+public class HelloWorld {
+   /* 这是第一个Java程序
+    * 它将输出 Hello World
+    * 这是一个多行注释的示例
+    */
+    public static void main(String[] args){
+       // 这是单行注释的示例
+       /* 这个也是单行注释的示例 */
+       System.out.println("Hello World"); 
+    }
+}
+import java.util.*;
+ 
+public class Test{
+ public static void main(String[] args) {
+     List<String> list=new ArrayList<String>();
+     list.add("Hello");
+     list.add("World");
+     list.add("HAHAHAHA");
+     //第一种遍历方法使用 For-Each 遍历 List
+     for (String str : list) {            //也可以改写 for(int i=0;i<list.size();i++) 这种形式
+        System.out.println(str);
+     }
+ 
+     //第二种遍历，把链表变为数组相关的内容进行遍历
+     String[] strArray=new String[list.size()];
+     list.toArray(strArray);
+     for(int i=0;i<strArray.length;i++) //这里也可以改写为  for(String str:strArray) 这种形式
+     {
+        System.out.println(strArray[i]);
+     }
+     
+    //第三种遍历 使用迭代器进行相关遍历
+     
+     Iterator<String> ite=list.iterator();
+     while(ite.hasNext())//判断下一个元素之后有值
+     {
+         System.out.println(ite.next());
+     }
+ }
+}
